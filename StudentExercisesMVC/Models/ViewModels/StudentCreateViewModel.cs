e9772236579ab8a695f2fe9498e529eca3d4e768
Grady Robbins <gradyrobbins13@gmail.com>
2019-04-11 15:15:58 -0700

    form renders

---
+++ StudentExercisesMVC/Models/ViewModels/StudentCreateViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExercisesMVC.Models.ViewModels
+{
+    public class StudentCreateViewModel
+    {
+        public StudentCreateViewModel()
+        {
+            Cohorts = new List<Cohort>();
+        }
+
+        public StudentCreateViewModel(string connectionString)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, CohortName from Cohort;";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Cohorts = new List<Cohort>();
+
+                    while (reader.Read())
+                    {
+                        Cohorts.Add(new Cohort
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            CohortName = reader.GetString(reader.GetOrdinal("CohortName"))
+                        });
+                    }
+                    reader.Close();
+                }
+            }
+        }
+
+
+        public Student Student { get; set; }
+        public List<Cohort> Cohorts { get; set; }
+        public String SlackHandle { get; set; }
+        public List<SelectListItem> CohortOptions
+        {
+            get
+            {
+                return  Cohorts.Select(c => new SelectListItem
+                {
+                    Value = c.Id.ToString(),
+                    Text = c.CohortName
+                }).ToList();
+            }
+        }
+    }
+}
